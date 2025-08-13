146ff6581a3a08cc15e7f9c82c96c0400b1b1a69
PashaBudzin <pashabudzin@proton.me>
2025-08-13 20:33:24 +0300

    feat: add submissions on presentation page

--- web/src/components/presentation.tsx
+++ web/src/components/presentation.tsx
@@ -1,4 +1,5 @@
 import { useAtom } from "jotai/react";
+import Avatar from "./avatar";
 import type { Submission } from "@/stores/game";
 import { submissionPresentationAtom } from "@/stores/game";
 import { roomAtom } from "@/stores/room";
@@ -7,12 +8,12 @@ export default function Presentation() {
     const [submissions] = useAtom(submissionPresentationAtom);
 
     return (
-        <div className="glass absolute inset-0 m-20 mx-5 flex justify-center p-10 lg:mx-40">
-            <div className="my-auto w-full lg:w-1/3">
-                <h1>Presentation</h1>
+        <div className="glass absolute inset-0 m-20 mx-5 flex flex-col justify-center p-10 lg:mx-40">
+            <h1 className="text-center">It's time to present!</h1>
+            <div className="mx-auto h-full w-full overflow-x-auto lg:w-1/2">
                 <div>
-                    {Object.keys(submissions).map((k) =>
-                        presentSubmission(submissions[k]),
+                    {Object.keys(submissions).map((k, i) =>
+                        presentSubmission(submissions[k], k, i),
                     )}
                 </div>
             </div>
@@ -20,11 +21,17 @@ export default function Presentation() {
     );
 }
 
-function presentSubmission(submission: Submission) {
+function presentSubmission(submission: Submission, from: string, key: number) {
     switch (submission.type) {
         // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
         case "text":
-            return <TextSubission submissionData={submission.data} />;
+            return (
+                <TextSubission
+                    submissionData={submission.data}
+                    key={key}
+                    from={from}
+                />
+            );
         default:
             throw new Error(
                 `this type of submission isn't supported ${submission.type}`,
@@ -33,15 +40,25 @@ function presentSubmission(submission: Submission) {
 }
 
 function TextSubission({
+    from,
     submissionData,
+    key,
 }: {
+    from: string;
     submissionData: Extract<Submission, { type: "text" }>["data"];
+    key: number;
 }) {
     const [room] = useAtom(roomAtom);
+    const fromUser = room?.users.find((u) => u.id == from);
+
+    if (!fromUser) return <></>;
 
     return (
-        <div className="w-full">
-            <p>{submissionData}</p>
+        <div className="glass mt-5 flex w-full p-4" key={key}>
+            <div className="h-20 w-20">
+                <Avatar seed={fromUser.name} />
+            </div>
+            <p className="my-auto text-center">{submissionData}</p>
         </div>
     );
 }
